f7c87e095e6fd7c05b959d8a041cbc7a6ef6ff40
memascoli <memascoli@gmail.com>
2019-11-16 22:06:15 -0300

    Entradas para as outras fases.

---
+++ Teste_SVG/Assets/Scenes/SceneEntrance.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneEntrance : MonoBehaviour
+{
+    public GameObject keyUpInterface; // Icone de Interação
+    public GameObject sceneChangeMessage; // Mensagem Base
+    public GameObject sceneMessageFase; // Mensagens
+
+    public int cenaAtual;
+    public int mudarParaCena;
+
+    void Update()
+    {
+        // Gerador de mensagem de aceitação para mudar de cena
+        // É perguntado ao jogador se ele deseja mudar de fase
+        if(Input.GetButtonDown("Interacao") && SceneChanger.triggered == true && !SceneChanger.underProcess)
+        {
+            sceneChangeMessage.SetActive(true); // liga o painel de mensagem
+            sceneMessageFase.SetActive(true); // liga o texto "especifico"
+            SceneChanger.underProcess = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D entrada)
+    {
+        if (entrada.gameObject.CompareTag("Player"))
+        {
+            keyUpInterface.SetActive(true);
+            SceneChanger.triggered = true;
+            SceneChanger._cenaAtual = cenaAtual;
+            SceneChanger._mudarParaCena = mudarParaCena;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D entrada)
+    {
+        if (entrada.gameObject.CompareTag("Player"))
+        {
+            keyUpInterface.SetActive(true);
+            SceneChanger.triggered = true;
+            SceneChanger._cenaAtual = cenaAtual;
+            SceneChanger._mudarParaCena = mudarParaCena;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D entrada)
+    {
+        if (entrada.gameObject.CompareTag("Player"))
+        {
+            keyUpInterface.SetActive(false);
+            SceneChanger.triggered = false;
+        }
+    }
+}
--- Teste_SVG/Assets/Tochas/FlameFlickering.cs
+++ Teste_SVG/Assets/Tochas/FlameFlickering.cs
@@ -1,89 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.Rendering.LWRP;
-
-public class FlameFlickering : MonoBehaviour
-{
-    public UnityEngine.Experimental.Rendering.LWRP.Light2D flameLight1;
-    public UnityEngine.Experimental.Rendering.LWRP.Light2D flameLight2;
-    public UnityEngine.Experimental.Rendering.LWRP.Light2D flameLight3;
-    public UnityEngine.Experimental.Rendering.LWRP.Light2D flameLight4;
-    public UnityEngine.Experimental.Rendering.LWRP.Light2D flameLight5;
-    public float flickeringFactor = 0.01f;
-    private float flickeringTime = 1.0f;
-    private int aleatoriedade;
-    // Start is called before the first frame update
-    void Start()
-    {
-        flameLight1.gameObject.SetActive(true); // ativa o objeto flame 1
-        flameLight2.gameObject.SetActive(true); // ativa o objeto flame 2
-        flameLight3.gameObject.SetActive(true); // ativa o objeto flame 3
-        flameLight4.gameObject.SetActive(true); // ativa o objeto flame 4
-        flameLight5.gameObject.SetActive(true); // ativa o objeto flame 5
-        flameLight1.enabled = true; // liga flame 1
-        flameLight2.enabled = false; // desliga flame 2
-        flameLight3.enabled = false; // desliga flame 3
-        flameLight4.enabled = false; // desliga flame 4
-        flameLight5.enabled = false; // desliga flame 5
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        flickeringTime -= flickeringFactor;
-        if(flickeringTime <= 0.0f)
-        {
-            aleatoriedade = Random.Range(0, 5);
-            flickeringTime = 1.0f;
-        }
-
-        switch (aleatoriedade)
-        {
-            case 0:
-                flameLight1.enabled = true; // liga flame 1
-                flameLight2.enabled = false; // desliga flame 2
-                flameLight3.enabled = false; // desliga flame 3
-                flameLight4.enabled = false; // desliga flame 4
-                flameLight5.enabled = false; // desliga flame 5
-            break;
-            case 1:
-                flameLight1.enabled = false; // desliga flame 1
-                flameLight2.enabled = true; // liga flame 2
-                flameLight3.enabled = false; // desliga flame 3
-                flameLight4.enabled = false; // desliga flame 4
-                flameLight5.enabled = false; // desliga flame 5
-            break;
-            case 2:
-                flameLight1.enabled = false; // desliga flame 1
-                flameLight2.enabled = false; // desliga flame 2
-                flameLight3.enabled = true; // liga flame 3
-                flameLight4.enabled = false; // desliga flame 4
-                flameLight5.enabled = false; // desliga flame 5
-            break;
-            case 3:
-                flameLight1.enabled = false; // desliga flame 1
-                flameLight2.enabled = false; // desliga flame 2
-                flameLight3.enabled = false; // desliga flame 3
-                flameLight4.enabled = true; // liga flame 4
-                flameLight5.enabled = false; // desliga flame 5
-            break;
-            case 4:
-                flameLight1.enabled = false; // desliga flame 1
-                flameLight2.enabled = false; // desliga flame 2
-                flameLight3.enabled = false; // desliga flame 3
-                flameLight4.enabled = false; // desliga flame 4
-                flameLight5.enabled = true; // liga flame 5
-            break;
-            default:
-                flameLight1.enabled = true; // desliga flame 1
-                flameLight2.enabled = false; // desliga flame 2
-                flameLight3.enabled = false; // desliga flame 3
-                flameLight4.enabled = false; // desliga flame 4
-                flameLight5.enabled = false; // desliga flame 5
-            break;
-        }
-    }
-}
+﻿ using UnityEngine;
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine.Experimental.Rendering.LWRP;
+ 
+ public class FlameFlickering : MonoBehaviour
+ {
+     public float MaxReduction;
+     public float MaxIncrease;
+     public float RateDamping;
+     public float Strength;
+     public bool StopFlickering;
+ 
+     private UnityEngine.Experimental.Rendering.LWRP.Light2D _lightSource;
+     private float _baseIntensity;
+     private bool _flickering;
+ 
+     public void Reset()
+     {
+         MaxReduction = 0.2f;
+         MaxIncrease = 0.2f;
+         RateDamping = 0.1f;
+         Strength = 300;
+     }
+ 
+     public void Start()
+     {
+         _lightSource = GetComponent<Light2D>();
+         if (_lightSource == null)
+         {
+             Debug.LogError("Flicker script must have a Light Component on the same GameObject.");
+             return;
+         }
+         _baseIntensity = _lightSource.intensity;
+         StartCoroutine(DoFlicker());
+     }
+ 
+     void Update()
+     {
+         if (!StopFlickering && !_flickering)
+         {
+             StartCoroutine(DoFlicker());
+         }
+     }
+ 
+     private IEnumerator DoFlicker()
+     {
+         _flickering = true;
+         while (!StopFlickering)
+         {
+             _lightSource.intensity = Mathf.Lerp(_lightSource.intensity, Random.Range(_baseIntensity - MaxReduction, _baseIntensity + MaxIncrease), Strength * Time.deltaTime);
+             yield return new WaitForSeconds(RateDamping);
+         }
+         _flickering = false;
+     }
+ }
