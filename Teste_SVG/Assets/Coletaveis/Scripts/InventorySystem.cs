dc04333a0dfe8176d0fa300de3f35f673edd88b6
memascoli <memascoli@gmail.com>
2019-11-17 16:15:20 -0300

    Sistema de Inventário dos Coletáveis Criado.

---
+++ Teste_SVG/Assets/Coletaveis/Scripts/InventorySystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public static int cachimbos = 0;
+    public static bool chave = false;
+    public static bool dashPower = false;
+    public static bool firePower = false;
+    public static bool fogPower = false;
+
+    void Update()
+    {
+        Debug.Log("Cachimbos: " + cachimbos);
+        Debug.Log("Chave? " + chave);
+        Debug.Log("Dash? " + dashPower);
+        Debug.Log("Fogo? " + firePower);
+        Debug.Log("Neblina? " + fogPower);
+    }
+
+}
