86fca8a2de76ef782bde8ba936585e8b20a541ff
memascoli <memascoli@gmail.com>
2019-11-22 13:42:00 -0300

    Efeitos em cada fase.

---
+++ Teste_SVG/Assets/VFX/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    public float timer = 5.0f;
+
+    public ParticleSystem particle1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Partcle());
+        
+    }
+
+    IEnumerator Partcle()
+    {
+        if(!particle1.isPlaying)
+        {
+            yield return new WaitForSeconds(5);
+            particle1.Play(); // turn the particle system on
+        }
+        
+    }
+}
