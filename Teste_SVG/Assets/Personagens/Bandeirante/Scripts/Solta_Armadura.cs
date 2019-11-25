6f237246cc311759fa82ae610f8ada138d99a364
Rhinkal <augusto.antunes@hotmail.com>
2019-11-25 01:40:06 -0300

    Importados packages de inimigos. Configurados PPU dos sprites para os prefabs para escalonarem para a nova cena.

---
+++ Teste_SVG/Assets/Personagens/Bandeirante/Scripts/Solta_Armadura.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solta_Armadura : MonoBehaviour
+{
+
+    public Rigidbody2D armadura;
+    public Rigidbody2D capacete;
+    public Rigidbody2D garrucha;
+    public Rigidbody2D facao;
+
+    public Transform solta_armadura;
+    public Transform solta_capacete;
+    public Transform solta_garrucha;
+    public Transform solta_facao;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+    }
+
+    public void SoltaArmadura()
+    {
+        Rigidbody2D armadura_solta = Instantiate(armadura, solta_armadura.transform.position, solta_armadura.transform.rotation);
+        if (transform.localScale.x == -1)
+        {
+            armadura_solta.transform.localScale = new Vector3(1.0f , 1.0f , 1.0f);
+            //armadura_solta.AddForce(Vector2.left * 5);
+        }
+        else if (transform.localScale.x == -1)
+        {
+            armadura_solta.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            //armadura_solta.AddForce(Vector2.right * 5);
+        }
+    }
+
+    public void SoltaCapacete()
+    {
+        Rigidbody2D capacete_solto = Instantiate(capacete, solta_capacete.transform.position, solta_capacete.transform.rotation);
+        if (transform.localScale.x == 1)
+        {
+            capacete_solto.AddForce(Vector2.left * 500);
+        }
+        else if (transform.localScale.x == -1)
+        {
+            capacete_solto.AddForce(Vector2.right * 500);
+        }
+    }
+    public void SoltaGarrucha()
+    {
+        Rigidbody2D garrucha_solta = Instantiate(garrucha, solta_garrucha.transform.position, solta_garrucha.transform.rotation);
+        if (transform.localScale.x == 1)
+        {
+            garrucha_solta.AddForce(Vector2.right * 500);
+        }
+        else if (transform.localScale.x == -1)
+        {
+            garrucha_solta.AddForce(Vector2.left * 500);
+        }
+    }
+    public void SoltaFacao()
+    {
+        Rigidbody2D facao_solto = Instantiate(facao, solta_facao.transform.position, solta_facao.transform.rotation);
+        if (transform.localScale.x == 1)
+        {
+            facao_solto.AddForce(Vector2.left * 100);
+        }
+        else if (transform.localScale.x == -1)
+        {
+            facao_solto.AddForce(Vector2.right * 100);
+        }
+    }
+}
---
+++ Teste_SVG/Assets/Personagens/Ranged/Scripts/Ranged_Tiro.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranged_Tiro : MonoBehaviour
+{
+    private Transform braco;
+    private Vector2 p_Pos;
+
+    [SerializeField]
+    private int vel;
+
+    [SerializeField]
+    private Rigidbody2D projetil;
+
+    [SerializeField]
+    private Transform emissor;
+
+    [SerializeField]
+    private Transform player;
+
+
+    private void Start()
+    {
+        braco = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        p_Pos = new Vector3(player.transform.position.x, player.transform.position.y);
+        braco.transform.right = p_Pos * -1;
+
+    }
+
+    public void Atira ()
+    {
+        Vector2 dir = new Vector2(emissor.position.x, emissor.position.y);
+        Rigidbody2D mao = Instantiate(projetil, emissor.position, emissor.rotation);
+        mao.AddForce(p_Pos.normalized * vel);
+    }
+
+}
