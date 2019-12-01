9f928dac6bcb33b089aa45802fb739e351c764eb
LeonardoTurella <leoturella98@gmail.com>
2019-12-01 03:39:09 -0300

    -Cheat de cachimbos e de hora do dia - Desbloqueáveis da oca funcionando - Polimento do interior da oca do player - Começo do ajuste da UI de coletáveis

--- Teste_SVG/Assets/Coletaveis/Scripts/Coletavel.cs
+++ Teste_SVG/Assets/Coletaveis/Scripts/Coletavel.cs
@@ -15,30 +15,31 @@ public class Coletavel : MonoBehaviour
                 transform.position = pullingArea.transform.position;
                 DataSystem.cachimbos++;
                 ColetavelUI.activated = true;
+                
             }
             else if(this.gameObject.CompareTag("Chave"))
             {
                 transform.position = pullingArea.transform.position;
                 DataSystem.chave = true;
-                ColetavelUI.activated = true;
+                
             }
             else if(this.gameObject.CompareTag("Dash"))
             {
                 transform.position = pullingArea.transform.position;
                 DataSystem.dashPower = true;
-                ColetavelUI.activated = true;
+                
             }
             else if(this.gameObject.CompareTag("Fire"))
             {
                 transform.position = pullingArea.transform.position;
                 DataSystem.firePower = true;
-                ColetavelUI.activated = true;
+                
             }
             else if(this.gameObject.CompareTag("Neblina"))
             {
                 transform.position = pullingArea.transform.position;
                 DataSystem.fogPower = true;
-                ColetavelUI.activated = true;
+                
             }
         }
     }
--- Teste_SVG/Assets/Coletaveis/Scripts/ColetavelUI.cs
+++ Teste_SVG/Assets/Coletaveis/Scripts/ColetavelUI.cs
@@ -11,7 +11,9 @@ public class  ColetavelUI : MonoBehaviour
 
         private float sizeX;
         private bool resize = false;
-        private Transform panelTransform;
+        public RectTransform panelTransform;
+
+    [SerializeField] private GameObject[] desbloqueios;
 
     void Start()
     {
@@ -25,12 +27,60 @@ public class  ColetavelUI : MonoBehaviour
             cachimboText.text = DataSystem.cachimbos + " / 16";
             Resizer();
         }
+
+        if (DataSystem.cachimbos == 2)
+        {
+            desbloqueios[0].SetActive(true);
+        }
+
+
+        if (DataSystem.cachimbos == 4)
+        {
+            desbloqueios[1].SetActive(true);
+        }
+
+
+        if (DataSystem.cachimbos == 6)
+        {
+            desbloqueios[2].SetActive(true);
+        }
+
+
+        if (DataSystem.cachimbos == 8)
+        {
+            desbloqueios[3].SetActive(true);
+        }
+
+
+        if (DataSystem.cachimbos == 10)
+        {
+            desbloqueios[4].SetActive(true);
+        }
+
+
+        if (DataSystem.cachimbos == 12)
+        {
+            desbloqueios[5].SetActive(true);
+        }
+
+
+        if (DataSystem.cachimbos == 16)
+        {
+            desbloqueios[6].SetActive(true);
+        }
+
     }
 
     void Resizer()
     {
+
+        Debug.Log("Resizer");
+        if (!panelTransform.gameObject.activeInHierarchy)
+        {
+            Desresizer();
+        }
         
-        if(!resize)
+       /* if(!resize)
         {
             panel.transform.localScale = new Vector2(Mathf.Clamp(sizeX, 0f, 1f), 1.0f);
             sizeX += 0.1f;
@@ -51,12 +101,14 @@ public class  ColetavelUI : MonoBehaviour
             sizeX = 0.0f;
             resize = false;
             activated = false;
-        }
+        }*/
     }
 
     IEnumerator Desresizer()
     {
-        yield return new WaitForSeconds(3);
-        resize = true;
+        Debug.Log("IfResizer");
+        panelTransform.gameObject.SetActive(true);
+        panelTransform.Translate(Vector3.right * Time.deltaTime);
+        yield return null;
     }
 }
