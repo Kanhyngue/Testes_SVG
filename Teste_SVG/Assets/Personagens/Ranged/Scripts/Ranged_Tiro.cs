a622cce5903d89895a88521eb1cdeb05a695245b
Rhinkal <augusto.antunes@hotmail.com>
2019-12-01 02:02:58 -0300

    Ajuste de PPU do tiro de inimigo ranged, correção de codigo para braço seguir jogador e do controlador para ativar com parametros

--- Teste_SVG/Assets/Personagens/Ranged/Scripts/Ranged_Tiro.cs
+++ Teste_SVG/Assets/Personagens/Ranged/Scripts/Ranged_Tiro.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Ranged_Tiro : MonoBehaviour
 {
     private Transform braco;
-    private Vector2 p_Pos;
+    
+    private Vector3 p_Pos;
 
     [SerializeField]
     private int vel;
@@ -19,24 +20,38 @@ public class Ranged_Tiro : MonoBehaviour
     [SerializeField]
     private Transform player;
 
+    private Animator _anim;
+
 
     private void Start()
     {
         braco = transform.GetChild(0);
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        p_Pos = new Vector3(player.transform.position.x, player.transform.position.y);
-        braco.transform.right = p_Pos * -1;
-
+        p_Pos = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z) ;
+        braco.transform.right = (p_Pos - braco.transform.position) * -1; 
     }
 
-    public void Atira ()
+    public void Atira()
     {
-        Vector2 dir = new Vector2(emissor.position.x, emissor.position.y);
-        Rigidbody2D mao = Instantiate(projetil, emissor.position, emissor.rotation);
-        mao.AddForce(p_Pos.normalized * vel);
+        Vector2 dir = new Vector2(-emissor.position.x, emissor.position.y);
+        Rigidbody2D mao = Instantiate(projetil, emissor.position, emissor.rotation.normalized);
+        /*        Transform _tMao = mao.GetComponent<Transform>();
+        */
+        mao.transform.position = Vector2.MoveTowards(mao.transform.position, p_Pos.normalized, vel);
+ //       mao.transform.rotation = new Quaternion(0,0,0,0);
+        mao.AddForce((p_Pos.normalized), ForceMode2D.Impulse);
+        Destroy.(mao, 2f);
     }
 
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _anim.SetTrigger("IsAiming");
+        }
+    }
 }
