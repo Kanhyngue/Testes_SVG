0def6f801c24018f8c200bb5411dff4a56ec54bc
memascoli <memascoli@gmail.com>
2019-12-05 12:31:15 -0300

    Músicas dos Bosses funcionando.

---
+++ Teste_SVG/Assets/BossDeath.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDeath : MonoBehaviour
+{
+    public GameObject mySelf;
+
+    private void Dead()
+    {
+        mySelf.SetActive(false);
+    }
+}
